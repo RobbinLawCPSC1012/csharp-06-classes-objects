69a5d9d4344c098655f5c0ea761778505ff07db3
Robbin Law <robbinl@telusplanet.net>
2019-11-11 13:08:45 -0700

    a

--- 04-list-of-objects-StudentManager/Program.cs
+++ 04-list-of-objects-StudentManager/Program.cs
@@ -10,6 +10,42 @@ namespace _04_list_of_objects_StudentManager
     {
         static void Main(string[] args)
         {
+            var students = new List<Student>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                var newStudent = new Student();
+
+                Console.Write("Student Name (string): ");
+                newStudent.Name = Console.ReadLine();
+
+                Console.Write("Student Grade (int): ");
+                newStudent.Grade = int.Parse(Console.ReadLine());
+
+                Console.Write("Student Birthday (yyyy-mm-dd): ");
+                newStudent.Birthday = Console.ReadLine();
+
+                students.Add(newStudent);
+
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() == "n")
+                    adding = false;
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, Grade: {student.Grade}, Birthday: {student.Birthday}");
+            }
         }
+    }
+
+    class Student
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
     }
 }
